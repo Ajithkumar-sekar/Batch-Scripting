dab95413fa4da6772ff0025142f6cf8cfa304dc1
Ajithkumar sekar <ajithkumarsekark@gmail.com>
2016-01-26 00:49:24 +0530

    Batch animation

---
+++ Animation.bat
@@ -0,0 +1,32 @@
+@echo off
+title Custom pause and Animation
+REM echo welcome %username%!
+echo Press any key!
+pause>nul
+cls
+echo Your content is processing.                     [-2%]
+PING 1.1.1.1 -n 1 -w 300>nul
+cls
+echo Your content is processing..                         [-14%]
+PING 1.1.1.1 -n 1 -w 300>nul
+cls
+echo Your content is processing...                        [-26%]
+PING 1.1.1.1 -n 1 -w 300>nul
+cls
+echo Your content is processing.....                      [-49%]
+PING 1.1.1.1 -n 1 -w 300>nul
+cls
+echo Your content is processing........                   [-69%]
+PING 1.1.1.1 -n 1 -w 300>nul
+cls
+echo Your content is processing............               [-72%]
+PING 1.1.1.1 -n 1 -w 300>nul
+cls
+echo Your content is processing..................         [-89%]
+PING 1.1.1.1 -n 1 -w 300>nul
+cls
+echo Your content is processing...........................[-100%]
+PING 1.1.1.1 -n 1 -w 300>nul
+echo Your content has processed
+echo Press any key to terminate!!
+pause>null
